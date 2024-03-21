2e2072296aa63e49659ab9c6edeef77ddf54bd00
khasonoff1 <khasonoff1@gmail.com>
2024-03-21 16:09:40 +0500

    3

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -3,8 +3,6 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { MdKeyboardVoice } from "react-icons/md";
-import { SiGooglelens } from "react-icons/si";
 
 const SearchComponent = () => {
     const [inputValue, setInputValue] = useState<string>("");
@@ -18,7 +16,7 @@ const SearchComponent = () => {
 
     return (
         <div className="text-center mt-16 mb-6">
-            <h1 className="text-6xl md:text-8xl mb-8 text-white font-bold">Google</h1>
+            <h1 className="text-6xl md:text-8xl mb-8 text-white font-bold">GOOGLE</h1>
             <form
                 className="bg-white rounded-md max-w-[500px] w-full mx-auto flex items-center justify-between py-3 px-6 gap-3 shadow-md"
                 onSubmit={handleSubmit}>
@@ -33,12 +31,6 @@ const SearchComponent = () => {
                     onChange={(e) => setInputValue(e.target.value)}
                     value={inputValue}
                 />
-                <div className="flex items-center gap-2">
-                    <button>
-                        <MdKeyboardVoice size={20} className="text-blue-500" />
-                    </button>
-                    <SiGooglelens size={20} className="text-blue-500" />
-                </div>
             </form>
         </div>
     );
